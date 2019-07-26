c31a9c8147efe2748191e383046a77ab8c4c0030
Nyota254 <knyota66@gmail.com>
2019-07-26 18:48:19 +0300

    add jQuery code into the script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
-
+function Order(size,toppings, number) {
+    this.size = size;
+    this.number = number;
+    this.toppings = toppings;
+}
 
 
 $(document).ready(function () {
+
+    
+    $(".sizeToppings1").submit(function () {
+
+        event.preventDefault();
+
+        var inputSize = $("input:radio[name=size]:checked").val();
+        var inputNumber = parseInt($("numberofpizzas").val());
+        var inputToppings = $("input:radio[name=toppings]:checked").val();
+        var newOrder = new Pizza(inputSize, inputToppings, inputNumber);
+        
+        alert(newOrder.size);
+
+    });
 
 });

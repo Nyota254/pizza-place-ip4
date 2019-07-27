a30f641b0130cf48f522e62fdd7dd5457c69b572
Nyota254 <knyota66@gmail.com>
2019-07-27 08:56:02 +0300

    add functionality to buttons of the individual pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -23,16 +23,46 @@ $(document).ready(function () {
         $("#toppingsorderd").text(newOrder.toppings);
         $("#numberorderd").text(newOrder.number);
 
+    });
+
+    $("button#Delivery").click(function(){
+        prompt("Please enter your location");
+        alert("Thank you your order will be Deliverd soon");
+    });
+
+    $("button#shop-collection").click(function(){
+        alert("Thank you for choosing us as your pizza place please come again");
+    });
+
+    $("form#sizeToppings2").submit(function (event) {
+
+        event.preventDefault();
+        var inputtype = $("#pizzatype2").val();
+        var inputSize = $("input:radio[name=size]:checked").val();
+        var inputNumber = parseInt($("input#numberofpizzas").val());
+        var inputToppings = $("input:radio[name=toppings]:checked").val();
+        var newOrder = new Order(inputtype, inputSize, inputToppings, inputNumber);
         
+        $("#pizzaorderd").text(newOrder.type);
+        $("#sizeorderd").text(newOrder.size);
+        $("#toppingsorderd").text(newOrder.toppings);
+        $("#numberorderd").text(newOrder.number);
 
-        $("button#Delivery").click(function(){
-            prompt("Please enter your location");
-            alert("Thank you your order will be Deliverd soon");
-        });
-        $("button#shop-collection").click(function(){
-            alert("Thank you for choosing us as your pizza place please come again");
-        });
+    });  
+    
+    $("form#sizeToppings3").submit(function (event) {
 
+        event.preventDefault();
+        var inputtype = $("#pizzatype3").val();
+        var inputSize = $("input:radio[name=size]:checked").val();
+        var inputNumber = parseInt($("input#numberofpizzas").val());
+        var inputToppings = $("input:radio[name=toppings]:checked").val();
+        var newOrder = new Order(inputtype, inputSize, inputToppings, inputNumber);
+        
+        $("#pizzaorderd").text(newOrder.type);
+        $("#sizeorderd").text(newOrder.size);
+        $("#toppingsorderd").text(newOrder.toppings);
+        $("#numberorderd").text(newOrder.number);
     });
 
 });

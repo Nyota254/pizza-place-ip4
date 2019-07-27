3cebe0e5da608a4ed7c9dd82696a7ae9bb4bde68
Nyota254 <knyota66@gmail.com>
2019-07-27 08:45:26 +0300

    add display of orders on the cart and also add functionality to the buttons of home delivery and shop-collection

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-function Order(size,toppings, number) {
+function Order(type,size,toppings, number) {
+    this.type = type;
     this.size = size;
     this.number = number;
     this.toppings = toppings;
@@ -11,14 +12,26 @@ $(document).ready(function () {
     $("form#sizeToppings1").submit(function (event) {
 
         event.preventDefault();
-
+        var inputtype = $("#pizzatype").val();
         var inputSize = $("input:radio[name=size]:checked").val();
         var inputNumber = parseInt($("input#numberofpizzas").val());
         var inputToppings = $("input:radio[name=toppings]:checked").val();
-        var newOrder = new Order(inputSize, inputToppings, inputNumber);
+        var newOrder = new Order(inputtype, inputSize, inputToppings, inputNumber);
         
+        $("#pizzaorderd").text(newOrder.type);
         $("#sizeorderd").text(newOrder.size);
+        $("#toppingsorderd").text(newOrder.toppings);
+        $("#numberorderd").text(newOrder.number);
 
+        
+
+        $("button#Delivery").click(function(){
+            prompt("Please enter your location");
+            alert("Thank you your order will be Deliverd soon");
+        });
+        $("button#shop-collection").click(function(){
+            alert("Thank you for choosing us as your pizza place please come again");
+        });
 
     });
 
